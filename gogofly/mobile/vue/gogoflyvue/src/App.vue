8f35b962fdd30c8304e0989655a69b6417943b0c
damon <damon@qq.com>
2026-01-07 16:46:07 +0800

    feat(setup): initialize Vue 3 project with TypeScript and Vite configuration

---
+++ gogofly/mobile/vue/gogoflyvue/src/App.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Description: 
+ * @Version: 
+ * @Author: damon
+ * @Date: 2026-01-07 16:31:33
+ * @LastEditors: damon
+ * @LastEditTime: 2026-01-07 16:43:50
+-->
+<script setup lang="ts">
+</script>
+
+<template>
+  damon
+</template>
+
+<style scoped>
+
+</style>
---
+++ gogofly/mobile/vue/gogoflyvue/src/main.ts
@@ -0,0 +1,12 @@
+/*
+ * @Description: 
+ * @Version: 
+ * @Author: damon
+ * @Date: 2026-01-07 16:31:33
+ * @LastEditors: damon
+ * @LastEditTime: 2026-01-07 16:43:19
+ */
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
